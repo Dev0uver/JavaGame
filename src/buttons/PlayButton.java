2a2ac6bcb3a421b76aea456fb006d9f5a0bdd76c
Dev0uver <atom.01@yandex.ru>
2023-06-04 13:09:19 +0300

    New class Entity that extended by all entities, almost all logic moved to the "Game" class, controls fixes, logic changes, getters and setters instead of static fields and different minor changes.

--- src/buttons/PlayButton.java
+++ src/buttons/PlayButton.java
@@ -3,18 +3,22 @@ package buttons;
 import buttons.Buttons;
 import main.GamePanel;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class PlayButton extends Buttons {
 
+    Image img = new ImageIcon("src/Assets/Sprites/Play.png").getImage();
+
 
     @Override
     public void RenderButton(Graphics graphics) { // отрисовка кнопки
-        graphics.setColor(Color.lightGray);
-        graphics.fillRect( xPosition, yPosition, width, height);
-        graphics.setColor(Color.BLACK);
-        graphics.setFont(font); // установка шрифта
-        graphics.drawString("Play", xPosition + 80, yPosition + 40); // рисование надписи в кнопке
+//        graphics.setColor(Color.lightGray);
+//        graphics.fillRect( xPosition, yPosition, width, height);
+//        graphics.setColor(Color.BLACK);
+//        graphics.setFont(font); // установка шрифта
+//        graphics.drawString("Play", xPosition + 80, yPosition + 40); // рисование надписи в кнопке
+            graphics.drawImage(img,xPosition, yPosition, null);
     }
 
     @Override
@@ -22,7 +26,7 @@ public class PlayButton extends Buttons {
         if (((xPosition <=x) & (x <=xPosition + width)) & ((yPosition <= y) & (y <= yPosition + height))){
 
             gamePanel.pauseFlag = false; // снятие флага паузы
-            if (gamePanel.flag){
+            if (!gamePanel.flag){
                 gamePanel.flag = true;
             }
 
